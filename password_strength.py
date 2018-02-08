f3d66198ed6cdb880d5161114f5c06661339a9c7
iriderpro <irider.pro@gmail.com>
2018-02-08 21:03:39 +0300

    Update password_strength.py

--- password_strength.py
+++ password_strength.py
@@ -1,5 +1,35 @@
 import re
-import password_blacklist
+
+
+list_bad_password = [
+    'Password1',
+    '12345',
+    '123',
+    '1',
+    '12',
+    '0',
+    '111',
+    '0000',
+    '1111',
+    '123456',
+    'Password',
+    '12345678',
+    'qwerty',
+    '123456789',
+    'letmein',
+    '1234567',
+    'football',
+    'iloveyou',
+    'admin',
+    'welcome',
+    'monkey',
+    'login',
+    'abc123',
+    'starwars',
+    '123123',
+    'passw0rd',
+    'hello',
+]
 
 
 def password_check(password):
@@ -12,7 +42,7 @@ def password_check(password):
         password,
     ) is None
     date_error = re.search(r"\d{6,}", password) is not None
-    if password in password_blacklist.list_password:
+    if password in list_bad_password:
         score = 0
     else:
         score = 4
@@ -31,4 +61,7 @@ def password_check(password):
 
 
 if __name__ == '__main__':
-    print(password_check(input('Введите пароль: ')))
+    print(
+        'Сложность пароля по шкале от 1 до 10 :',
+        password_check(input('Введите пароль: '))
+    )
