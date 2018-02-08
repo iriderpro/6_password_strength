b7d7c76e843ac9af7aa9ac1d0131625ae0b31223
iriderpro <irider.pro@gmail.com>
2018-02-08 20:21:09 +0300

    Update password_strength.py

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,34 @@
+import re
+import password_blacklist
 
 
-def get_password_strength(password):
-    pass
+def password_check(password):
+    length_error = len(password) < 8
+    digit_error = re.search(r"\d.", password) is None
+    uppercase_error = re.search(r"[A-Z]", password) is None
+    lowercase_error = re.search(r"[a-z]", password) is None
+    symbol_error = re.search(
+        r"[ !@#$%&'()*+,-./[\\\]^_`{|}~" + r'"]',
+        password,
+    ) is None
+    date_error = re.search(r"\d{6,}", password) is not None
+    if password in password_blacklist.list_password:
+        score = 0
+    else:
+        score = 4
+
+    for item in (
+        length_error,
+        digit_error,
+        uppercase_error,
+        lowercase_error,
+        symbol_error,
+        date_error,
+    ):
+        if item is False:
+            score += 1
+    return score
 
 
 if __name__ == '__main__':
-    pass
+    print(password_check(input('Введите пароль: ')))
