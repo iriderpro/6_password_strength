4215ba86adcea00ba8ce653d4166469459df8001
iriderpro <irider.pro@gmail.com>
2018-02-15 16:20:16 +0300

    Update password_strength.py

--- password_strength.py
+++ password_strength.py
@@ -1,69 +1,40 @@
 import re
-
-
-list_bad_password = [
-    'Password1',
-    '12345',
-    '123',
-    '1',
-    '12',
-    '0',
-    '111',
-    '0000',
-    '1111',
-    '123456',
-    'Password',
-    '12345678',
-    'qwerty',
-    '123456789',
-    'letmein',
-    '1234567',
-    'football',
-    'iloveyou',
-    'admin',
-    'welcome',
-    'monkey',
-    'login',
-    'abc123',
-    'starwars',
-    '123123',
-    'passw0rd',
-    'hello',
-]
+import password_blacklist
+import getpass
 
 
 def password_check(password):
-    length_check = len(password) < 8
+    minimal_length_password = 8
+    length_check = len(password) < minimal_length_password
     digit_check = re.search(r"\d.", password) is None
     uppercase_check = re.search(r"[A-Z]", password) is None
     lowercase_check = re.search(r"[a-z]", password) is None
     symbol_check = re.search(
-        r"[ !@#$%&'()*+,-./[\\\]^_`{|}~" + r'"]',
+        r"[ !#$%&'()*+,-./[\\\]^_`{|}~" + r'"]',
         password,
     ) is None
     date_number_check = re.search(r"\d{6,}", password) is not None
-    if password in list_bad_password:
-        score = 0
+    if password in password_blacklist.list_bad_password:
+        score1 = 0
     else:
         if length_check is True:
-            score = 0
+            score1 = 0
         else:
-            score = 5
-
-    for check in (
+            score1 = 5
+    score2 = sum([
         digit_check,
         uppercase_check,
         lowercase_check,
         symbol_check,
-        date_number_check,
-    ):
-        if check is False:
-            score += 1
+        date_number_check])
+    score2 = 5 - score2
+    score = score1 + score2
+    print(score)
     return score
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(
-        'Сложность пароля по шкале от 1 до 10 :',
-        password_check(input('Введите пароль: '))
+        "Сложность пароля по шкале от 1 до 10 :",
+        password_check(getpass.getpass(prompt="Введите пароль : "))
     )
